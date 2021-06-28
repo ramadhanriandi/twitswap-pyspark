e7929e25e6dad5d4c37deb123243cbbc53dd786e
ramadhanriandi <izurbudianto@gmail.com>
2021-06-28 22:00:37 +0700

    Process consumed tweet streams and count each tweet types

---
+++ main.py
@@ -0,0 +1,57 @@
+import json
+import pyspark
+from pyspark import SparkContext, SparkConf
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+from types import SimpleNamespace
+
+def count_tweet_types(data):
+  if hasattr(data, 'referenced_tweets'):
+    tweet_type = data.referenced_tweets[0].type
+    if tweet_type == "retweeted":
+      return ("retweet", 1)
+    elif tweet_type == "quoted":
+      return ("quote", 1)
+    elif tweet_type == "replied_to":
+      return ("replied_to", 1)
+
+  return ("tweet", 1)
+  
+def process_lines(lines):
+    tweets = lines.map(lambda obj: obj[1]).filter(lambda line: len(line) >= 11)
+    objects = tweets.map(lambda tweet: json.loads(tweet, object_hook = lambda d: SimpleNamespace(**d)))
+    datas = objects.map(lambda obj: obj.data) 
+
+    # Count for each tweet types (tweet, retweet, quote, reply)
+    converted_tweets = datas.map(count_tweet_types)
+    tweet_types = converted_tweets.reduceByKey(lambda a, b: a + b)
+
+    return tweet_types
+
+# Environment variables
+APP_NAME = "TwitSwap - PySpark"
+MASTER = "local"
+
+KAFKA_TOPIC = "raw-tweet-topic"
+BOOTSTRAP_SERVER = "localhost:9092"
+
+# Spark configurations
+conf = SparkConf() \
+    .setAppName(APP_NAME) \
+    .setMaster(MASTER)
+sc = SparkContext.getOrCreate(conf=conf)
+
+ssc = StreamingContext(sc, 10) # stream each ten second
+ssc.checkpoint("./checkpoint")
+
+# Consume Kafka topic
+lines = KafkaUtils.createDirectStream(ssc, [KAFKA_TOPIC], {"metadata.broker.list": BOOTSTRAP_SERVER})
+
+# Process lines retrieved from Kafka topic
+result = process_lines(lines)
+
+# Print the result
+result.pprint()
+
+ssc.start()
+ssc.awaitTermination()
